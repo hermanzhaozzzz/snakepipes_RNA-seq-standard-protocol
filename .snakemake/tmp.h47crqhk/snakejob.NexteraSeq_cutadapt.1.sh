eb3af326c9a45590a1a6e1e3dd393eeb47dd0862
hermanzhaozzzz <hermanzhaozzzz@gmail.com>
2021-02-02 17:07:07 +0800

    fix snakefile main

---
+++ .snakemake/tmp.h47crqhk/snakejob.NexteraSeq_cutadapt.1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# properties = {"type": "single", "rule": "NexteraSeq_cutadapt", "local": false, "input": ["../fastq/pcif1-WT-1_R1.fq.gz", "../fastq/pcif1-WT-1_R2.fq.gz"], "output": ["../fix.fastq/293T-RNASeq-pcif1-WT-1_R1_cutadapt.fq.gz", "../fix.fastq/293T-RNASeq-pcif1-WT-1_R2_cutadapt.fq.gz"], "wildcards": {"sample": "pcif1-WT-1"}, "params": {}, "log": ["../fix.fastq/293T-RNASeq-pcif1-WT-1_cutadapt.log"], "threads": 1, "resources": {}, "jobid": 1, "cluster": {}}
+ cd /gpfs/user/zhaohuanan/3.project/2021_Other_projects/2021_CTT/snakepipes_RNA-seq-standard-protocol && \
+PATH='/home/zhaohuanan/zhaohn_HD/miniconda3/bin':$PATH /home/zhaohuanan/zhaohn_HD/miniconda3/bin/python3.8 \
+-m snakemake ../fix.fastq/293T-RNASeq-pcif1-WT-1_R1_cutadapt.fq.gz --snakefile /gpfs/user/zhaohuanan/3.project/2021_Other_projects/2021_CTT/snakepipes_RNA-seq-standard-protocol/Snakefile.py \
+--force -j --keep-target-files --keep-remote --max-inventory-time 0 \
+--wait-for-files /gpfs/user/zhaohuanan/3.project/2021_Other_projects/2021_CTT/snakepipes_RNA-seq-standard-protocol/.snakemake/tmp.h47crqhk ../fastq/pcif1-WT-1_R1.fq.gz ../fastq/pcif1-WT-1_R2.fq.gz --latency-wait 60 \
+ --attempt 1 --force-use-threads --scheduler ilp \
+\
+\
+--wrapper-prefix https://github.com/snakemake/snakemake-wrappers/raw/ \
+  -p --allowed-rules NexteraSeq_cutadapt --nocolor --notemp --no-hooks --nolock \
+--mode 2  && exit 0 || exit 1
+
--- Snakefile_main.py
+++ Snakefile_main.py
@@ -2,12 +2,9 @@
 
 ########################################################################
 # ZHAO Huanan
-# 2020-05-09
-# 293T RNA-Seq analysis pipeline
+# 2021-02-01
+# pcif1 RNA-Seq analysis pipeline
 ######################################################################## 
-# run on abyss
-# /home/zhaohuanan/zhaohn_HD/3.project/4.2020-05_BE3andBE4_RNA-seq_merge_KJY/snakepipes_cutadapt-STARmapping-FPKM-sortBAM
-
 # before this, make sure you have done fastqc + multiqc 
 # https://github.com/hermanzhaozzzz/snakepipes_fastqc-multiqc
 # and add trim rule to make a better trim
@@ -18,43 +15,28 @@
 # 2. STAR alingment 
 # 3. samtools addreplacerg - > RG
 # 4. samtools sort by position
-# 5. cufflinks calculate FPKM
-# 6. picard mark duplicates
-# 7. samtools build bam index
-# 常规RNAseq的mapping和细节处理算到这里就可以了
-# call SNP使用另一个snakefile
+# 5. picard mark duplicates
+# 6. samtools build bam index
 # --------------------------------------------------------------->>>>>>>
 # software
 # --------------------------------------------------------------->>>>>>>
-GATK4 = "/home/zhaohuanan/zhaohn_HD/1.apps/GATK4/gatk-4.1.7.0/gatk-package-4.1.7.0-local.jar"
-
-
-# cutadapt v1.18
-# conda create -n cutadapt python=3.6 cutadapt=1.18
-CUTADAPT = "~/anaconda3/envs/cutadapt/bin/cutadapt"
-
-
-# --------------------------------------------------------------->>>>>>>
-# use conda to create a env contains below apps
-# --------------------------------------------------------------->>>>>>>
-# STAR --version 2.7.2b
-STAR = "~/anaconda3/envs/snakepipes_cutadapt-STARmapping-FPKM-sortBAM/bin/STAR"
-
-
-# JAVA version 1.8
-JAVA = "~/anaconda3/envs/snakepipes_cutadapt-STARmapping-FPKM-sortBAM/bin/java"
-
+# make sure the fastqqc and the multiqc are in you PATH
+# get the application path
+with os.popen("which cutadapt") as path:
+    CUTADAPT = path.read().strip()
+    print('PATH cutadapt:', CUTADAPT)
+with os.popen("which samtools") as path:
+    SAMTOOLS = path.read().strip()
+    print('PATH samtools:', SAMTOOLS)    
+with os.popen("which STAR") as path:
+    STAR = path.read().strip()
+    print('PATH star:', STAR)   
+with os.popen("which java") as path:
+    JAVA = path.read().strip()
+    print('PATH java:', JAVA)
 
 # picard version 2.23
-PICARD = "~/anaconda3/envs/snakepipes_cutadapt-STARmapping-FPKM-sortBAM/bin/../share/picard-2.23.1-0/picard.jar"
-
-
-# cufflinks 
-CUFFLINKS = "~/anaconda3/envs/snakepipes_cutadapt-STARmapping-FPKM-sortBAM/bin/cufflinks"
-
-
-# samtools v1.10
-SAMTOOLS = "~/anaconda3/envs/snakepipes_cutadapt-STARmapping-FPKM-sortBAM/bin/samtools"
+PICARD = "/home/zhaohuanan/zhaohn_HD/1.apps/picard/picard.jar"
 
 
 # --------------------------------------------------------------->>>>>>>
@@ -62,17 +44,28 @@ SAMTOOLS = "~/anaconda3/envs/snakepipes_cutadapt-STARmapping-FPKM-sortBAM/bin/sa
 # --------------------------------------------------------------->>>>>>>
 HG38_FA = "/home/zhaohuanan/zhaohn_HD/2.database/bwa_hg38/hg38_only_chromosome.fa"
 HG38_FA_DICT = "/home/zhaohuanan/zhaohn_HD/2.database/bwa_hg38/hg38_only_chromosome.fa"
-# STAR --version 2.7.2b
 STAR_HG38_INDEX = "/home/zhaohuanan/zhaohn_HD/2.database/star_hg38"
 HG38_GTF = "/home/zhaohuanan/zhaohn_HD/2.database/annotate_hg38/20200714_ComprehensiveGeneAnnotation_Chr_gencode.v29.annotation.gtf"
+
+# 这里是小鼠的, 没改变量名
+# HG38_FA = "/home/zhaohuanan/zhaohn_HD/2.database/db_genomes/genome_fa/genome_gencode_GRCm38.p6/GRCm38.p6.genome.fa"
+# HG38_FA_DICT = "/home/zhaohuanan/zhaohn_HD/2.database/db_genomes/genome_fa/genome_gencode_GRCm38.p6/GRCm38.p6.genome.fa"
+# STAR_HG38_INDEX = "/home/zhaohuanan/zhaohn_HD/2.database/db_genomes/genome_fa/genome_gencode_GRCm38.p6/star_index_150bp"
+# HG38_GTF = "/home/zhaohuanan/zhaohn_HD/2.database/db_genomes/genome_annotation/genome_gencode_GRCm38.p6/gencode.vM25.annotation.gtf"
 
 
 # --------------------------------------------------------------->>>>>>>
 # vars
 # --------------------------------------------------------------->>>>>>>
 SAMPLES = [
-    "BE4-0706-rep1",
-    "M2-0706-rep1"
+    "pcif1-WT-1",
+    "pcif1-WT-2",
+    "pcif1-WT-3",
+    "pcif1-WT-4",
+    "pcif1-KO-1",
+    "pcif1-KO-2",
+    "pcif1-KO-3",
+    "pcif1-KO-4"
 #    "test"
 ]
 
@@ -82,8 +75,8 @@ SAMPLES = [
 # ------------------------------------------------------------------------------------------>>>>>>>>>>
 rule all:
     input:
-        expand("../fix.fastq/293T-RNASeq-{sample}_R1.fastq.gz",sample=SAMPLES),
-        expand("../fix.fastq/293T-RNASeq-{sample}_R2.fastq.gz",sample=SAMPLES),
+        expand("../fastq/{sample}_R1.fq.gz",sample=SAMPLES),
+        expand("../fastq/{sample}_R2.fq.gz",sample=SAMPLES),
         expand("../fix.fastq/293T-RNASeq-{sample}_R1_cutadapt.fq.gz",sample=SAMPLES),
         expand("../fix.fastq/293T-RNASeq-{sample}_R2_cutadapt.fq.gz",sample=SAMPLES),
         expand("../bam/293T-RNASeq-{sample}_Aligned.out.bam",sample=SAMPLES),
@@ -91,7 +84,7 @@ rule all:
         expand("../bam/293T-RNASeq-{sample}_Aligned_sort.bam",sample=SAMPLES),
         expand("../bam/293T-RNASeq-{sample}_Aligned_sort_MarkDup.bam",sample=SAMPLES),
         expand("../bam/293T-RNASeq-{sample}_Aligned_sort.bam.bai",sample=SAMPLES),
-        expand("../fpkm/{sample}",sample=SAMPLES)
+#         expand("../fpkm/{sample}",sample=SAMPLES)
 # ------------------------------------------------------------------------------------------>>>>>>>>>>
 # cutadapter
 # ------------------------------------------------------------------------------------------>>>>>>>>>>
@@ -112,6 +105,22 @@ rule TruSeq_cutadapt:
         -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT \
         -o {output[0]} -p {output[1]} {input[0]} {input[1]} > {log} 2>&1
         """
+# rule NexteraSeq_cutadapt:
+#     input:
+#         "../fastq/{sample}_R1.fq.gz",
+#         "../fastq/{sample}_R2.fq.gz"
+#     output:
+#         "../fix.fastq/293T-RNASeq-{sample}_R1_cutadapt.fq.gz",
+#         "../fix.fastq/293T-RNASeq-{sample}_R2_cutadapt.fq.gz"
+#     log:
+#         "../fix.fastq/293T-RNASeq-{sample}_cutadapt.log"
+#     shell:# using illumina NexteraSeq adaptor
+#         """
+#         {CUTADAPT} -j 24 --times 1  -e 0.1  -O 3  --quality-cutoff 25 \
+#         -m 55 -a TCGTCGGCAGCGTCAGATGTGTATAAGAGACAG \
+#         -A GTCTCGTGGGCTCGGAGATGTGTATAAGAGACAG \
+#         -o {output[0]} -p {output[1]} {input[0]} {input[1]} > {log} 2>&1
+#         """
 # ------------------------------------------------------------------------------------------>>>>>>>>>>
 # STAR mapping
 # ------------------------------------------------------------------------------------------>>>>>>>>>>
@@ -127,7 +136,6 @@ rule STAR_mapping:
         "../bam/293T-RNASeq-{sample}_"
     shell:
         """
-        srun -T 24 -c 24 \
         {STAR} \
         --genomeDir {STAR_HG38_INDEX} \
         --runThreadN 24 \
@@ -150,7 +158,6 @@ rule add_RG_tag:
         tag = "'@RG\\tID:{sample}\\tSM:{sample}\\tPL:ILLUMINA'"
     shell:
         """
-        srun -T 24 -c 24 \
         {SAMTOOLS} addreplacerg -r {params.tag} -@ 24 -O BAM -o {output} --reference {HG38_FA_DICT} {input}
         """
 # ------------------------------------------------------------------------------------------>>>>>>>>>>
@@ -163,7 +170,6 @@ rule BAM_sort_by_position:
         "../bam/293T-RNASeq-{sample}_Aligned_sort.bam"
     shell:
         """
-        srun -T 24 -c 24 \
         {SAMTOOLS} sort \
         -O BAM \
         -o {output} \
@@ -185,8 +191,7 @@ rule BAM_mark_duplicate:
         "../bam/293T-RNASeq-{sample}_Aligned_sort_MarkDup.log"
     shell:
         """
-        srun -T 24 -c 24 \
-        {JAVA} -Xms90g -Xmx90g -XX:ParallelGCThreads=24 \
+        {JAVA} -Xms100g -Xmx100g -XX:ParallelGCThreads=24 \
         -jar {PICARD} MarkDuplicates \
         I={input} \
         O={output[0]} \
@@ -203,29 +208,7 @@ rule BAM_index:
         "../bam/293T-RNASeq-{sample}_Aligned_sort.bam.bai"
     shell:
         """
-        srun -T 24 -c 24 \
         {SAMTOOLS} index -@ 24 \
         {input} \
         {output}
         """
-# ------------------------------------------------------------------------------------------>>>>>>>>>>
-# cufflinks calculate FPKM for calling RNA CBE off target
-# https://github.com/hermanzhaozzzz/jupyter_call-RNA-CBE-off-target
-# ------------------------------------------------------------------------------------------>>>>>>>>>>     
-rule cufflinks_FPKM:
-    input:
-        "../bam/293T-RNASeq-{sample}_Aligned_sort.bam"
-    output:
-        directory('../fpkm/{sample}')
-    log:
-        "../fpkm/{sample}.log"
-    shell:
-        """
-        srun -T 24 -c 24 \
-        {CUFFLINKS} -p 24 --library-type fr-firststrand \
-        -G {HG38_GTF} \
-        -o {output} \
-        {input} 2>{log}
-        """
-# 常规RNAseq的mapping和细节处理算到这里就可以了
-# call SNP使用另一个snakefile
